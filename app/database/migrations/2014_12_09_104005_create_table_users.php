6fe126a87fb4dd6657c2c1550d93da71f0d7a703
Andrea Passaglia <gurghet@gmail.com>
2014-12-09 11:44:27 +0100

    Apparently the users table was missing, ADDED!

---
+++ app/database/migrations/2014_12_09_104005_create_table_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+            $table->string('username');
+            $table->string('password');
+            $table->string('role');
+            $table->primary('id');
+            $table->unique('username');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
